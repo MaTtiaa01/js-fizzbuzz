03abae4cba586fa9ee3b184f8920e8ca687c594a
MaTtiaa01 <mattia2001@live.it>
2022-10-10 18:44:31 +0100

    add bonus file

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,43 @@
+//Scrivi un programma che stampi in console i numeri da 1 a 100,
+
+//queryselector
+//.innerHTML +=
+// Will evaluate to true if the variable is divisible by 2
+//variable % 2 === 0 
+
+let row = document.querySelector(".row");
+
+
+for (let i = 1; i <= 100; i++) {
+
+    //ma che per i multipli di 3 stampi “Fizz” al posto del numero
+   if (i % 3 === 0){
+      const element = `<div class="col box box_fizz"> Fizz </div>   `
+
+      row.innerHTML += element;
+      console.log(element);
+
+    //e per i multipli di 5 stampi “Buzz”.
+   } else if(i % 5 === 0){
+    const element = `<div class="col box box_buzz"> Buzz </div>   `
+
+    row.innerHTML += element;
+    console.log(element);
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+   } else if (i % 3 === 0 & i % 5 === 0)  {
+    const element = `<div class="col box box_fizzbuzz"> FizzBuzz </div>   `
+
+    row.innerHTML += element;
+    console.log(element);
+   }
+   else{
+    const element = `<div class="col box box_number"> ${i}</div>   `
+
+    row.innerHTML += element;
+    console.log(element);
+   }
+    
+}
+
+
+
