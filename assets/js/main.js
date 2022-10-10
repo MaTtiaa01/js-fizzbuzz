c8680468b3f9c28d4096cfb8beebbd9267a4218f
MaTtiaa01 <mattia2001@live.it>
2022-10-10 18:28:06 +0100

    add main structure js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,40 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100,
 
+//queryselector
+//.innerHTML +=
+// Will evaluate to true if the variable is divisible by 2
+//variable % 2 === 0 
+
+let row = document.querySelector(".row");
 
 
 for (let i = 1; i <= 100; i++) {
-   
+
+    //ma che per i multipli di 3 stampi “Fizz” al posto del numero
+   if (i % 3 === 0){
+      const element = `<div class="col box box${i}"> Fizz </div>   `
+
+      row.innerHTML = element;
+
+    //e per i multipli di 5 stampi “Buzz”.
+   } else if(i % 5 === 0){
+    const element = `<div class="col box box${i}"> Buzz </div>   `
+
+    row.innerHTML = element;
+
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+   } else if (i % 3 === 0 & i % 5 === 0)  {
+    const element = `<div class="col box box${i}"> FizzBuzz </div>   `
+
+    row.innerHTML = element;
+   }
+   else{
+    const element = `<div class="col box box${i}"> ${i}</div>   `
+
+    row.innerHTML = element;
+   }
     
 }
 
-//ma che per i multipli di 3 stampi “Fizz” al posto del numero
 
-//e per i multipli di 5 stampi “Buzz”.
-//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
