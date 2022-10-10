521d25cc2dbdab400cde296594baa33bbe5c02c4
MaTtiaa01 <mattia2001@live.it>
2022-10-10 18:14:07 +0100

    add html structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100,
 
 
+
+for (let i = 1; i <= 100; i++) {
+   
+    
+}
+
 //ma che per i multipli di 3 stampi “Fizz” al posto del numero
 
 //e per i multipli di 5 stampi “Buzz”.
