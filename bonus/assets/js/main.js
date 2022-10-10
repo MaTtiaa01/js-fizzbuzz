9889fb7681e94e2e13665a5cbde37a07ac2a1bfa
MaTtiaa01 <mattia2001@live.it>
2022-10-10 18:46:49 +0100

    add the bonus structure with append

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -8,35 +8,38 @@
 let row = document.querySelector(".row");
 
 
+
+
+
 for (let i = 1; i <= 100; i++) {
 
-    //ma che per i multipli di 3 stampi “Fizz” al posto del numero
-   if (i % 3 === 0){
-      const element = `<div class="col box box_fizz"> Fizz </div>   `
+   //ma che per i multipli di 3 stampi “Fizz” al posto del numero
+  if (i % 3 === 0){
+     const element = `<div class="col box box_fizz"> Fizz </div>   `
 
-      row.innerHTML += element;
-      console.log(element);
+     row.append(element) ;
+     console.log(element);
 
-    //e per i multipli di 5 stampi “Buzz”.
-   } else if(i % 5 === 0){
-    const element = `<div class="col box box_buzz"> Buzz </div>   `
+   //e per i multipli di 5 stampi “Buzz”.
+  } else if(i % 5 === 0){
+   const element = `<div class="col box box_buzz"> Buzz </div>   `
 
-    row.innerHTML += element;
-    console.log(element);
-    //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-   } else if (i % 3 === 0 & i % 5 === 0)  {
-    const element = `<div class="col box box_fizzbuzz"> FizzBuzz </div>   `
+    row.append(element) ;
+   console.log(element);
+   //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+  } else if (i % 3 === 0 & i % 5 === 0)  {
+   const element = `<div class="col box box_fizzbuzz"> FizzBuzz </div>   `
 
-    row.innerHTML += element;
-    console.log(element);
-   }
-   else{
-    const element = `<div class="col box box_number"> ${i}</div>   `
+    row.append(element) ;
+   console.log(element);
+  }
+  else{
+   const element = `<div class="col box box_number"> ${i}</div>   `
 
-    row.innerHTML += element;
-    console.log(element);
-   }
-    
+    row.append(element) ;
+   console.log(element);
+  }
+   
 }
 
 
